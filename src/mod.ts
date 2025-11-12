6d7127a52f4aecfd178523c8a873ab0b558550f1
Max P. <mail@0xMax42.io>
2025-11-12 14:21:59 +0100

    feat: export errors, interfaces, types, and utils from main module

--- src/mod.ts
+++ src/mod.ts
@@ -2,3 +2,15 @@
 export { HttpKernel } from './HttpKernel.ts';
 export { RouteBuilder } from './RouteBuilder.ts';
 export { createRouteMatcher } from './Utils/createRouteMatcher.ts';
+
+// Errors
+export * from './Errors/mod.ts';
+
+// Interfaces
+export * from './Interfaces/mod.ts';
+
+// Types
+export * from './Types/mod.ts';
+
+// Utils
+export * from './Utils/mod.ts';
